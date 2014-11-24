d5145f6122aaacb9cbd96ac1e9b9a07096d1c4b9
Mhel DC <mheldc@users.noreply.github.com>
2014-11-24 14:08:17 +0800

    Format code

--- gchatv2/app.js
+++ gchatv2/app.js
@@ -25,14 +25,14 @@ io.sockets.on('connection', function(socket)
     				   };
 					
 		http.get(options, function(res){
-						res.statusCode;
-						res.setEncoding('utf8');
-						res.on('data', function(chunk){
-							var JSONobj = JSON.parse(chunk);
-										if(JSONobj['status'] == 200 && JSONobj['code'] == 'user_authenticated'){
-														uservalidated = true;
-										}
-						});
+			res.statusCode;
+			res.setEncoding('utf8');
+			res.on('data', function(chunk){
+				var JSONobj = JSON.parse(chunk);
+							if(JSONobj['status'] == 200 && JSONobj['code'] == 'user_authenticated'){
+											uservalidated = true;
+							}
+			});
 		}).on('error', function(e){
 			console.log('Got an error : ' + e.message);
 		});
@@ -41,15 +41,15 @@ io.sockets.on('connection', function(socket)
 		socket.join(ud.cid);
 		io.in(ud.cid).emit('allow-chat-input',{allow: uservalidated, cid: ud.cid});
 		if (ud.username.indexOf('Guest') < 0) {
-						io.in(ud.cid).emit('update-ui', {msgtype: 'notification', user: ud.username, msg: 'You have successfully connected to Channel ' + ud.cid, cid:ud.cid});		
+			io.in(ud.cid).emit('update-ui', {msgtype: 'notification', user: ud.username, msg: 'You have successfully connected to Channel ' + ud.cid, cid:ud.cid});		
 		} else {
-						io.in(ud.cid).emit('update-ui', {msgtype: 'notification', user: ud.username, msg: 'Welcome ' + ud.username + ' to Channel ' + ud.cid, cid:ud.cid});
+			io.in(ud.cid).emit('update-ui', {msgtype: 'notification', user: ud.username, msg: 'Welcome ' + ud.username + ' to Channel ' + ud.cid, cid:ud.cid});
 		}
 	});
 				
 	socket.on('send-gm', function(ud){
-					console.log(ud.username + ' is sending a message : [' + ud.msg + '] in channel ' + ud.cid);
-					io.in(ud.cid).emit('update-ui', {msgtype: 'chatmessage', user: ud.username, msg: ud.msg, uavatar: ud.uavatar, cid:ud.cid});
+		console.log(ud.username + ' is sending a message : [' + ud.msg + '] in channel ' + ud.cid);
+		io.in(ud.cid).emit('update-ui', {msgtype: 'chatmessage', user: ud.username, msg: ud.msg, uavatar: ud.uavatar, cid:ud.cid});
 	});
 	
 	socket.on('send-pm', function(ud){
@@ -61,8 +61,8 @@ io.sockets.on('connection', function(socket)
 	});
 	
 	socket.on('disconnect', function(ud){
-					socket.broadcast.emit('update-ui', 'SERVER', socket.uname + ' leaves the chatroom');
-					socket.leave(ud.cid);
+		socket.broadcast.emit('update-ui', 'SERVER', socket.uname + ' leaves the chatroom');
+		socket.leave(ud.cid);
 	});
 				
 });
