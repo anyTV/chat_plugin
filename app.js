ea0efcdc200b0b623b73ddf9ec737443c37b5e1f
Mhel DC <romeldelacruz@gmail.com>
2015-01-07 17:17:03 +0800

    Bug Ref#201501070001
    Additional fix when another user views or watch same streams that cause
    chat input box to "black out"

--- app.js
+++ app.js
@@ -16,65 +16,51 @@ console.log(res.statusCode);
 
 io.sockets.on('connection', function(socket)
 {
-				socket.on('auth_user', function(ud){
-								console.log(ud);
-								var options = {
-																host:'54.255.176.250',
-																path:'http://54.255.176.250/authenticate?access='+ud.access_code+'&user='+ud.userid
-															};
-								console.log(options);
-								
-								http.get(options, function(res){
-												res.statusCode;
-												res.setEncoding('utf8');
-												res.on('data', function(chunk){
-													var JSONobj = JSON.parse(chunk);
-																console.log(res.statusCode);
-																console.log('User ' + ud.username + ' joins in channel ' + ud.cid);
-																socket.join(ud.cid);
-																
-																//if(JSONobj['status'] == 200 && JSONobj['code'] == 'user_authenticated'){
-																if(res.statusCode == 200 && JSONobj['code'] == 'user_authenticated'){                           
-																				io.in(ud.cid).emit('allow-chat-input',{allow: true, cid: ud.cid});
-																				//io.in(ud.cid).emit('update-ui', {msgtype: 'notification', user: ud.username, msg: 'You have successfully connected to Channel ' + ud.cid, cid:ud.cid});
-																} else {
-																				io.in(ud.cid).emit('allow-chat-input',{allow: false, cid: ud.cid});
-																				//io.in(ud.cid).emit('update-ui', {msgtype: 'notification', user: ud.username, msg: 'Welcome ' + ud.username + ' to Channel ' + ud.cid, cid:ud.cid});
-																}
-																
-												});
-								}).on('error', function(e){
-									console.log('Got an error : ' + e.message);
-								});
-								
-								//console.log('User ' + ud.username + ' joins in channel ' + ud.cid);
-								//socket.join(ud.cid);
-								//
-								//if (ud.username.indexOf('Guest') < 0) {
-								//				io.in(ud.cid).emit('update-ui', {msgtype: 'notification', user: ud.username, msg: 'You have successfully connected to Channel ' + ud.cid, cid:ud.cid});         
-								//} else {
-								//				io.in(ud.cid).emit('update-ui', {msgtype: 'notification', user: ud.username, msg: 'Welcome ' + ud.username + ' to Channel ' + ud.cid, cid:ud.cid});
-								//}
-								
-				});
-				
-				socket.on('send-gm', function(ud){
-								console.log(ud.username + ' is sending a message : [' + ud.msg + '] in channel ' + ud.cid);
-								io.in(ud.cid).emit('update-ui', {msgtype: 'chatmessage', user: ud.username, msg: ud.msg, uavatar: ud.uavatar, cid:ud.cid});
-				});
-				
-				socket.on('send-pm', function(ud){
-						
-				});
-				
-				socket.on('leave-room', function(ud){
-						
-				});
-				
-				socket.on('disconnect', function(ud){
-								socket.broadcast.emit('update-ui', 'SERVER', socket.uname + ' leaves the chatroom');
-								socket.leave(ud.cid);
-				});
+    socket.on('auth_user', function(ud){
+        console.log(ud);
+        var options =   {
+                            host:'54.255.176.250',
+                            path:'http://54.255.176.250/authenticate?access='+ud.access_code+'&user='+ud.userid
+                        };
+        
+        http.get(options, function(res){
+                        res.statusCode;
+                        res.setEncoding('utf8');
+                        res.on('data', function(chunk){
+                            var JSONobj = JSON.parse(chunk);
+                                        console.log(res.statusCode);
+                                        console.log('User ' + ud.username + ' joins in channel ' + ud.cid);
+                                        socket.join(ud.cid);
+                                        
+                                        //if(JSONobj['status'] == 200 && JSONobj['code'] == 'user_authenticated'){
+                                        if(res.statusCode === 200  && JSONobj['code'] == 'user_authenticated') {                           
+                                                        socket.emit('allow-chat-input',{allow: true, cid: ud.cid});
+                                        } else {
+                                                        socket.emit('allow-chat-input',{allow: false, cid: ud.cid});
+                                        }
+                        });
+        }).on('error', function(e){
+            console.log('Got an error : ' + e.message);
+        });         
+    });
+
+    socket.on('send-gm', function(ud){
+                    console.log(ud.username + ' is sending a message : [' + ud.msg + '] in channel ' + ud.cid);
+                    socket.in(ud.cid).emit('update-ui', {msgtype: 'chatmessage', user: ud.username, msg: ud.msg, uavatar: ud.uavatar, cid:ud.cid});
+    });
+    
+    socket.on('send-pm', function(ud){
+            
+    });
+    
+    socket.on('leave-room', function(ud){
+            
+    });
+    
+    socket.on('disconnect', function(ud){
+                    socket.broadcast.emit('update-ui', 'SERVER', socket.uname + ' leaves the chatroom');
+                    socket.leave(ud.cid);
+    });
 				
 });
 
