713c40a7bda251e6ec5890f92395efb0b7617fd9
Mhel DC <romeldelacruz@gmail.com>
2015-01-07 19:49:21 +0800

    Bug Ref#201501070002
    Applied fix for chat message does not display to sender.

--- app.js
+++ app.js
@@ -24,44 +24,45 @@ io.sockets.on('connection', function(socket)
                         };
         
         http.get(options, function(res){
-                        res.statusCode;
-                        res.setEncoding('utf8');
-                        res.on('data', function(chunk){
-                            var JSONobj = JSON.parse(chunk);
-                                        console.log(res.statusCode);
-                                        console.log('User ' + ud.username + ' joins in channel ' + ud.cid);
-                                        socket.join(ud.cid);
-                                        
-                                        //if(JSONobj['status'] == 200 && JSONobj['code'] == 'user_authenticated'){
-                                        if(res.statusCode === 200  && JSONobj['code'] == 'user_authenticated') {                           
-                                                        socket.emit('allow-chat-input',{allow: true, cid: ud.cid});
-                                        } else {
-                                                        socket.emit('allow-chat-input',{allow: false, cid: ud.cid});
-                                        }
-                        });
+            res.statusCode;
+            res.setEncoding('utf8');
+            res.on('data', function(chunk){
+                var JSONobj = JSON.parse(chunk);
+                    console.log(res.statusCode);
+                    console.log('User ' + ud.username + ' joins in channel ' + ud.cid);
+                    socket.join(ud.cid);
+                    
+                    //if(JSONobj['status'] == 200 && JSONobj['code'] == 'user_authenticated'){
+                    if(res.statusCode === 200  && JSONobj['code'] == 'user_authenticated') {                           
+                                    socket.emit('allow-chat-input',{allow: true, cid: ud.cid});
+                    } else {
+                                    socket.emit('allow-chat-input',{allow: false, cid: ud.cid});
+                    }
+            });
         }).on('error', function(e){
             console.log('Got an error : ' + e.message);
-        });         
+        });
+       
     });
 
     socket.on('send-gm', function(ud){
-                    console.log(ud.username + ' is sending a message : [' + ud.msg + '] in channel ' + ud.cid);
-                    socket.in(ud.cid).emit('update-ui', {msgtype: 'chatmessage', user: ud.username, msg: ud.msg, uavatar: ud.uavatar, cid:ud.cid});
+        console.log(ud.username + ' is sending a message : [' + ud.msg + '] in channel ' + ud.cid);
+        io.in(ud.cid).emit('update-ui', {msgtype: 'chatmessage', user: ud.username, msg: ud.msg, uavatar: ud.uavatar, cid:ud.cid});
     });
     
     socket.on('send-pm', function(ud){
-            
+       
     });
     
     socket.on('leave-room', function(ud){
-            
+        
     });
     
     socket.on('disconnect', function(ud){
-                    socket.broadcast.emit('update-ui', 'SERVER', socket.uname + ' leaves the chatroom');
-                    socket.leave(ud.cid);
+        socket.broadcast.emit('update-ui', 'SERVER', socket.uname + ' leaves the chatroom');
+        socket.leave(ud.cid);
     });
-				
+	
 });
 
 server.listen(app.get('port'), function(){
